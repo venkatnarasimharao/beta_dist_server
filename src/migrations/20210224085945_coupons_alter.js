2015a09eb15aa04f3b2e6b6c18dc8de365f76c68
uppu540 <upendra.giduturi540@outlook.com>
2021-02-24 20:31:50 +0530

    ch

--- src/migrations/20210224085945_coupons_alter.js
+++ src/migrations/20210224085945_coupons_alter.js
@@ -1,8 +1,10 @@
 exports.up = function (knex) {
     try {
         return knex.schema
-            .raw(`ALTER TABLE coupons_user ADD if not exists coupons_id INT NULL DEFAULT NULL AFTER email;`)
-            .raw(`ALTER TABLE coupons_user ADD if not exists coupon_status TINYINT(1) NOT NULL DEFAULT '1' AFTER coupon_code;`)
+            .alterTable('coupons_user', (table) => {
+                table.integer('coupons_id').defaultTo(null).after('email');
+                table.tinyint('coupon_status', 1).defaultTo(1).after('coupon_code');
+            })
             .raw(`ALTER TABLE users CHANGE name name VARCHAR(191) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL, CHANGE email email VARCHAR(191) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL, CHANGE provider provider VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;`)
     } catch (err) {
         console.log(err, 'error in migrations')
